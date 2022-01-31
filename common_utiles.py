75434fd44151c905d300545f7a11d52dd6d5d149
Amannor <alon.mannor@gmail.com>
2022-01-31 19:40:26 +0200

    Auto formatted the file

--- common_utiles.py
+++ common_utiles.py
@@ -16,7 +16,7 @@ BASE_OUT_DIR = "out"
 DATA_FOLDER = "dataverse_files"
 FINAL_REPORT_FOLDER = "final_report"
 FINAL_REPORT_DATA_FOLDER = os.path.join(FINAL_REPORT_FOLDER, "data")
-DATE_FORMAT = "%Y-%m-%d" #i.e: "YYYY-MM-DD"
+DATE_FORMAT = "%Y-%m-%d"  # i.e: "YYYY-MM-DD"
 MAX_IDS_ALLOWED_BY_TWITTER = 100
 RECORDS_PER_FILE = 10 ** 5
 
@@ -29,11 +29,13 @@ OTHER_STANCE_COLOR = "green"
 SEARCH_URL = "https://api.twitter.com/2/tweets"
 BOT_SCORES_DF = None
 
+
 class Sentiment(Enum):
     NEUTRAL = 0
     REMAIN = 1
     LEAVE = 2
     OTHER = 99
+
 
 def remove_bots_by_threshold(df, bot_score_threshold):
     df.set_index("user_id", inplace=True)
@@ -48,24 +50,30 @@ def remove_bots_by_threshold(df, bot_score_threshold):
     df.drop(columns=["bot_score"], inplace=True)
     return df
 
+
 def handle_bots(bot_score_threshold):
     should_filter_bots = False
     bot_msg_suffix = ""
     global BOT_SCORES_DF
     if not bot_score_threshold is None:
         if not 0 <= bot_score_threshold <= 1:
-            warnings.warn(f'Bot score should be probability (between 0 and 1) but got {bot_score_threshold} - ignoring it')
+            warnings.warn(
+                f'Bot score should be probability (between 0 and 1) but got {bot_score_threshold} - ignoring it')
         else:
             bot_msg_suffix = f" (bot score threshold {bot_score_threshold})"
             should_filter_bots = True
             if BOT_SCORES_DF is None:
                 full_fname = os.path.join(DATA_FOLDER, "users_stance_sentiment_botscore_tweetcounts.csv")
                 print(f'{get_cur_formatted_time()} Reading {full_fname}')
-                BOT_SCORES_DF = pd.read_csv(full_fname, sep="~", names=["user_id", "user_sentiment", "user_stance", "bot_score", "bot_fetch_time", "tweets_num"])
-                BOT_SCORES_DF.drop(columns=["user_sentiment", "user_stance", "bot_fetch_time", "tweets_num"], inplace=True)
+                BOT_SCORES_DF = pd.read_csv(full_fname, sep="~",
+                                            names=["user_id", "user_sentiment", "user_stance", "bot_score",
+                                                   "bot_fetch_time", "tweets_num"])
+                BOT_SCORES_DF.drop(columns=["user_sentiment", "user_stance", "bot_fetch_time", "tweets_num"],
+                                   inplace=True)
                 BOT_SCORES_DF = BOT_SCORES_DF[~BOT_SCORES_DF['bot_score'].isin([np.nan])]
 
     return should_filter_bots, bot_msg_suffix
+
 
 def df_to_csv_plus_create_dir(df, outdir, file_name):
     if not os.path.exists(outdir):
@@ -80,8 +88,10 @@ def save_fig(f_name, f_format="png"):
     plt.savefig(path)
     plt.clf()
 
-def get_cur_formatted_time(format = "%Y-%m-%d %H:%M:%S"):
+
+def get_cur_formatted_time(format="%Y-%m-%d %H:%M:%S"):
     return datetime.datetime.now().strftime(format)
+
 
 def write_to_json_file_if_not_empty(data, fname, add_epoch_suffix=False, escape_html_chars=True):
     if len(data) == 0:
@@ -98,6 +108,7 @@ def write_to_json_file_if_not_empty(data, fname, add_epoch_suffix=False, escape_
         with open(fname, "w", encoding="utf-8") as write_file:
             json.dump(data, write_file, indent=4, ensure_ascii=escape_html_chars)
 
+
 def get_trinary_sentiment(cur_stance, cur_sentiment):
     if cur_stance == "remain":
         return Sentiment.REMAIN
@@ -105,12 +116,14 @@ def get_trinary_sentiment(cur_stance, cur_sentiment):
         return Sentiment.LEAVE
     return Sentiment.OTHER
 
+
 def connect_to_endpoint(params, headers):
     response = requests.request("GET", SEARCH_URL, headers=headers, params=params)
     if response.status_code != 200:
         print(f'{get_cur_formatted_time()} {response.status_code}: {response.text}')
         return response.status_code
     return response.json()
+
 
 def get_existing_ids(out_dir):
     existing_ids = set()
@@ -135,8 +148,8 @@ def get_existing_ids(out_dir):
 
     return existing_ids
 
-def request_tweets_ids_from_csv(data_fname, bearer_token, out_dir, request_text=True, skip_first_line=False):
 
+def request_tweets_ids_from_csv(data_fname, bearer_token, out_dir, request_text=True, skip_first_line=False):
     data_file = os.path.join(DATA_FOLDER, data_fname)
     existing_ids = get_existing_ids(out_dir)
     print(f'{get_cur_formatted_time()} Found {len(existing_ids)} existing tweets ids (dir {out_dir})')
@@ -175,8 +188,6 @@ def request_tweets_ids_from_csv(data_fname, bearer_token, out_dir, request_text=
                 ids_not_found_fname = os.path.join(out_dir, "tweet_ids_not_found", "tweet_ids_not_found.json")
                 ids_not_authorized_fname = os.path.join(out_dir, "tweet_ids_not_authorized",
                                                         "tweet_ids_not_authorized.json")
-
-
 
                 for i, cur_tweets_in_requests in enumerate(tweets_ids_per_request):
                     query_params = {'ids': ",".join(cur_tweets_in_requests),
@@ -246,6 +257,7 @@ def request_tweets_ids_from_csv(data_fname, bearer_token, out_dir, request_text=
                             write_to_json_file_if_not_empty(tweet_ids_not_authorized, ids_not_authorized_fname, True)
                             tweet_ids_not_authorized = []
                 cur_tweets_ids = []
+
 
 def write_csv_file_if_data_not_empty(fname, data, header):
     if len(data) == 0:
